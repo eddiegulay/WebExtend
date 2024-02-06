38627b35ebced566f446515666d9d960a65a09b5
eddygulled <edgargulay69@gmail.com>
2024-02-06 20:30:09 +0300

    navigation controls

--- app/src/main/java/aida/inteli/webextend/MainActivity.java
+++ app/src/main/java/aida/inteli/webextend/MainActivity.java
@@ -1,10 +1,15 @@
 package aida.inteli.webextend;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,7 +25,11 @@ public class MainActivity extends AppCompatActivity {
         // hide the action bar
         getSupportActionBar().hide();
 
+        // Show splash screen initially
+        findViewById(R.id.splashScreen).setVisibility(View.VISIBLE);
+
         webView = findViewById(R.id.webView);
+
 
         // Enable JavaScript (optional)
         WebSettings webSettings = webView.getSettings();
@@ -34,9 +43,36 @@ public class MainActivity extends AppCompatActivity {
         // Set up WebViewClient to handle link clicks within the WebView
         webView.setWebViewClient(new MyWebViewClient());
 
-        // Load a website
-        webView.loadUrl("https://fimobile-extend.onrender.com");
 
+        // Before loading the website, check network connection
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Load the website
+            webView.loadUrl("https://fimobile-extend.onrender.com");
+
+            // Hide splash screen after a delay (e.g., 5 seconds)
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    findViewById(R.id.splashScreen).setVisibility(View.GONE);
+                }
+            }, 5000);
+        } else {
+            // Toast long message
+             Toast.makeText(this, "No internet connection, Please check your network  and try again", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Check if the WebView can go back in history
+        if (webView.canGoBack()) {
+            webView.goBack();  // Go back in WebView history
+        } else {
+            super.onBackPressed();  // If no history, proceed with the default back button behavior (close the app)
+        }
     }
 
     private class MyWebViewClient extends WebViewClient {
